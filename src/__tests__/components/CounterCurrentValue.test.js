c677551d4bc2b80830ac28a38fdedd81a788472e
harshmons <harshverma04111989@gmail.com>
2020-08-04 22:29:45 +0530

    Minor changes in test case

--- src/__tests__/components/CounterCurrentValue.test.js
+++ src/__tests__/components/CounterCurrentValue.test.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import CounterCurrentValue from '../../components/CounterCurrentValue';
 
-
-it('should renders `Counter current value` text with passed counter prop', () => {
-  const { queryByText,debug } = render(<CounterCurrentValue counterValue="4" />);
-  const text = queryByText(/Counter current value/i);
-  expect(text).toHaveTextContent("Counter current value : 4")
-});
-
-it('should have correct class', () => {
+describe("<CounterCurrentValue />", () => {
+  test('should renders `Counter current value` text with passed counter prop', () => {
     const { queryByText,debug } = render(<CounterCurrentValue counterValue="4" />);
     const text = queryByText(/Counter current value/i);
-    
-    // These are present in @testing-library/jest-dom/extend-expect
-    // When we are not using plain css
-    expect(text).toHaveClass("currentValueContainer")
-    
-    // When we are using css-modules or css-in-jss
-    expect(text).toHaveAttribute('class', expect.stringContaining('Value'))
-});
+    expect(text).toHaveTextContent("Counter current value : 4")
+  });
+
+  test('should have correct class', () => {
+      const { queryByText,debug } = render(<CounterCurrentValue counterValue="4" />);
+      const text = queryByText(/Counter current value/i);
+      
+      // These are present in @testing-library/jest-dom/extend-expect
+      // When we are not using plain css
+      expect(text).toHaveClass("currentValueContainer")
+      
+      // When we are using css-modules or css-in-jss
+      expect(text).toHaveAttribute('class', expect.stringContaining('Value'))
+  });
+})
+  
+
 
